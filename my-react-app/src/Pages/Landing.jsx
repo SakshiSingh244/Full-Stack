d25be4a17a8beff4e6eeb9947ed715b46f4bd863
Janvi Chandra <janvichandra4@gmail.com>
2024-07-20 13:57:25 +0530

    Landing Added

---
+++ my-react-app/src/Pages/Landing.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Hero from '../Components/Hero';
+import SecondSection from '../Components/SecondSection';
+import NotConvinced from '../Components/NotConvinced';
+import ChildPerformance from '../Components/ChildPerformance';
+import Testimonial from '../Components/Testimonial';
+// import Stats from '../Components/Stats';
+
+const Landing = () => {
+  return (
+    <div>
+      <Hero />
+      {/* <Stats /> */}
+      <SecondSection />
+      <ChildPerformance />
+      <Testimonial />
+      <NotConvinced />
+    </div>
+  )
+}
+
+export default Landing

2b214cde90cd7e8d0884632a2e9bd9670483b266
Janvi Chandra <janvichandra4@gmail.com>
2024-07-20 22:02:33 +0530

    Book Trial Class Page added

---
+++ my-react-app/src/Pages/BookTrialClass.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BookTrial from '../Components/BookTrial';
+
+const BookTrialClass = () => {
+  return (
+    <div>
+      <BookTrial />
+    </div>
+  )
+}
+
+export default BookTrialClass

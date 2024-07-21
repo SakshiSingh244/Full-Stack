b125794e0403dcf490fb539c1d0214470896faee
Sakshi Singh <singhsakshi200424@gmail.com>
2024-07-22 00:46:31 +0530

    Contact Page enhanced

--- my-react-app/src/Components/ContactComp.jsx
+++ my-react-app/src/Components/ContactComp.jsx
@@ -7,23 +7,24 @@ const ContactComp = () => {
     <div className="contact-main-container">
       <div className="contact-left-container">
         <div className="contact-upper-container">
-          <h2 className='ContactHead'>Contact Us</h2><p1 className='ContactSub'>Have some queries? Feel free to contact us</p1>
+          <h2 className="ContactHead">Contact Us</h2>
+          <p className="ContactSub">Have some queries? Feel free to contact us</p>
         </div>
         <div className="contact-lower-container">
           <form>
-            <div className="form-group">
+            <div className="contact-form">
               <label htmlFor="name">Name</label>
               <input type="text" id="name" name="name" placeholder="Enter Name" />
             </div>
-            <div className="form-group">
+            <div className="contact-form">
               <label htmlFor="phone">Phone No.</label>
               <input type="text" id="phone" name="phone" placeholder="Enter Phone No." />
             </div>
-            <div className="form-group">
+            <div className="contact-form">
               <label htmlFor="message">Message</label>
               <textarea id="message" name="message" placeholder="Enter Message"></textarea>
             </div>
-            <button type="submit" id="con">Submit</button>
+            <button id = "con" type="submit">Submit</button>
           </form>
         </div>
       </div>

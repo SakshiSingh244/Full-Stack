8200219e19e50a558738835d1ae8d325a36e8cdd
Sakshi Singh <singhsakshi200424@gmail.com>
2024-07-21 23:32:15 +0530

    contact page new

---
+++ my-react-app/src/Pages/Contactemp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContactComp from '../Components/ContactComp';
+import '../Components/ContactComp.css';
+
+const Contact = () => {
+  return (
+    <div className="contact-container">
+      <ContactComp />
+    </div>
+  );
+};
+
+export default Contact;

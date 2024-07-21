ea4e821580ca8fc4fa08f3e0a1edf0b389cda196
Janvi Chandra <janvichandra4@gmail.com>
2024-07-21 10:20:55 +0530

    Minor Changes in enhancement of UI

--- my-react-app/src/Components/Hero.jsx
+++ my-react-app/src/Components/Hero.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import './Hero.css';
 import HeroImg from '../assets/HeroImg.png';
 import '../index.css'
+import {Link} from 'react-router-dom';
 
 const Hero = () => {
   return (
-    <nav className="hero">
+    <div className="hero">
       <div className="hero-content">
         <div className="left-side">
           <h1 className='heading'>Unlock Your Child's Potential with Expert <span>Math Tutoring</span></h1>
           <p className='hero-para'>Personalized, offline classes for 5th-10th  graders with over 10 years of proven success.</p>
-          <button className='hero-button'>Book Trial Class</button>
+          <Link to="/bookclass"><button className='hero-button'>Book Trial Class</button></Link>
         </div>
         <div className="right-side">
           <img
@@ -20,7 +21,7 @@ const Hero = () => {
           />
         </div>
       </div>
-    </nav>
+    </div>
   );
 };
 
--- my-react-app/src/Components/NotConvinced.jsx
+++ my-react-app/src/Components/NotConvinced.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import './NotConvinced.css'
+import React from 'react';
+import './NotConvinced.css';
+import { Link } from 'react-router-dom';
 
 const NotConvinced = () => {
   return (
     <div>
-      <div className="container">
-        <h2 className='not-convinced'>Still Not Convinced?</h2>
-        <p className='not-convinced-para'>Experience the difference for yourself! Book a trial class now and see how we can help you excel in math. Have questions? We're here to help!</p>
+      <div className="ConvincedContainer">
+        <h2 className="not-convinced">Still Not Convinced?</h2>
+        <p className="not-convinced-para">Experience the difference for yourself! Book a trial class now and see how we can help you excel in math. Have questions? We're here to help!</p>
         <div className="buttons">
+          <Link to="/bookclass">
             <button className="book-trial-class">Book Trial Class</button>
+          </Link>
+          <Link to="/contact">
             <button className="contact">Contact</button>
+          </Link>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NotConvinced
+export default NotConvinced;
